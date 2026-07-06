3947bc5b3917b0a06cd36bd4dc53a670a3e9959d
edgaar <edgar@gmail.com>
2026-07-06 14:54:05 -0600

    ya le agregre las cosas de cuenta plataforma y usiario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,7 @@
-public class Usuario {
+public class Usuario extends CuentaUsuario {
+
+    public Usuario(String correoElectronico, int mesesActivos,PlanSuscripcion plan){
+        super(correoElectronico,mesesActivos,plan);
+    }
+
 }
